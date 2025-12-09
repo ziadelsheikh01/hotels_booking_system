daaf2c743e118fc6b7c879e3c48a34e718f70547
ziad elsheikh <ziad.elsheikh01@gmail.com>
2025-12-09 12:59:10 +0200

    create project files

---
+++ src/test/java/com/example/hotelmanagmentsystem/HotelmanagmentsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hotelmanagmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelmanagmentsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
